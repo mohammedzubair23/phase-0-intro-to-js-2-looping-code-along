af124cc391709ddca4b9ad1590eec010a1b7c14d
Mohammed Zubair <mohammedzubair10@yahoo.com>
2021-12-16 12:54:52 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, giftType) {
+  let newArray = [];
+
+  for (let i = 0; i < names.length; i++) {
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+  }
+
+  return newArray;
+}
+function countDown() {
+
+  for (let countDown = 10; countDown >= 0; countDown--) {
+    console.log(countDown);
+  }
+
+}
+    
